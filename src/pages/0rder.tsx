51c3e36cf6282b60248249471ae11591cc103d30
mariaOchoa <mariaochoa@seven4n.com>
2021-05-24 21:15:35 -0500

    Fix: layout in the order component

--- src/pages/0rder.tsx
+++ src/pages/0rder.tsx
@@ -1,7 +1,12 @@
+import Layout from "../components/Layout/Layout";
 import PizzaMaker from "../components/PizzaMaker/PizzaMaker";
 
 const Order = () => {
-  return <PizzaMaker />;
+  return (
+    <Layout>
+      <PizzaMaker />
+    </Layout>
+  );
 };
 
 export default Order;
